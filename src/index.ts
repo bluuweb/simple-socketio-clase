846f71260ebd8849c87e5e492ecad9583124d49b
bluuweb <contacto@bluuweb.cl>
2025-02-13 08:42:48 -0300

    Agregar soporte para variables de entorno y actualizar la conexión de Socket.io a la URL de producción

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import http from "node:http";
 import { Server } from "socket.io";
@@ -33,6 +34,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
